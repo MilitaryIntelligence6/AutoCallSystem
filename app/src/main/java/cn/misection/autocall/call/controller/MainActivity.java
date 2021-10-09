e1666db1a66f9f7d92940d7e887cedce01e0f800
JavaMan <static_void_main@foxmail.com>
2021-10-09 23:30:24 +0800

    修改字体;

--- app/src/main/java/cn/misection/autocall/call/controller/MainActivity.java
+++ app/src/main/java/cn/misection/autocall/call/controller/MainActivity.java
@@ -3,6 +3,7 @@ package cn.misection.autocall.call.controller;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,6 +25,7 @@ import java.util.concurrent.TimeUnit;
 
 import cn.misection.autocall.R;
 import cn.misection.autocall.databinding.ActivityMainBinding;
+import cn.misection.util.oututil.system.AppSystem;
 
 /**
  * @author javaman
@@ -38,11 +40,15 @@ public class MainActivity extends AppCompatActivity {
 
     private TelephonyManager mTelephonyManager;
 
+    private ScheduledExecutorService mMakePhoneCallThreadPool = Executors.newSingleThreadScheduledExecutor();
+
+    private SharedPreferences mPreferences;
+
     private long mPeriod = 10;
 
     private long mNextCallCountDown = 1;
 
-    private ScheduledExecutorService mMakePhoneCallThreadPool = Executors.newSingleThreadScheduledExecutor();
+    private long mLastExitTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +57,21 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
+    @Override
+    public void onBackPressed() {
+        if ((System.currentTimeMillis() - mLastExitTime) > 2000) {
+            AppSystem.out.printt(this, "再按一次返回键退出 App");
+            mLastExitTime = System.currentTimeMillis();
+        } else {
+            AppSystem.out.printt(this, "Bye!");
+            super.onBackPressed();
+        }
+    }
+
     private void init() {
         initBinding();
-        initState();
         initController();
+        initState();
         initActionListener();
     }
 
@@ -62,11 +79,15 @@ public class MainActivity extends AppCompatActivity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 
-    private void initState() {
+    private void initController() {
+        mPreferences = getPreferences(MODE_PRIVATE);
     }
 
-    private void initController() {
+    private void initState() {
+        mBinding.telphoneNumEditText.setText(mPreferences.getString("phoneNum", ""));
 
+        mPeriod = mPreferences.getLong("period", 10);
+        mBinding.periodEditText.setText(String.valueOf(mPeriod));
     }
 
     private void initActionListener() {
@@ -101,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCallButtonClicked(View view) {
+        savePhoneNum();
         // 检查是否获得了权限（Android6.0运行时权限）
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -108,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             //拨打电话
             Intent intent = new Intent(Intent.ACTION_CALL);
-            String phoneNum = String.valueOf(mBinding.phoneCallStateTextView.getText());
+            String phoneNum = String.valueOf(mBinding.telphoneNumEditText.getText()).trim();
             Log.d(TAG, "即将拨打的电话信息" + phoneNum);
             Uri data = Uri.parse("tel:" + phoneNum);
             intent.setData(data);
@@ -123,14 +145,13 @@ public class MainActivity extends AppCompatActivity {
 
     private void applyPermission() {
         // 没有获得授权，申请授权
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                Manifest.permission.CALL_PHONE)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CALL_PHONE)) {
             // 返回值：
-            //如果app之前请求过该权限,被用户拒绝, 这个方法就会返回true.
-            //如果用户之前拒绝权限的时候勾选了对话框中”Don’t ask again”的选项,那么这个方法会返回false.
-            //如果设备策略禁止应用拥有这条权限, 这个方法也返回false.
+            // 如果app之前请求过该权限,被用户拒绝, 这个方法就会返回true.
+            // 如果用户之前拒绝权限的时候勾选了对话框中”Don’t ask again”的选项,那么这个方法会返回false.
+            // 如果设备策略禁止应用拥有这条权限, 这个方法也返回false.
             // 弹窗需要解释为何需要该权限，再次请求授权
-            Toast.makeText(MainActivity.this, "请授权！", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "请授权！", Toast.LENGTH_LONG).show();
             // 帮跳转到该应用的设置界面，让用户手动授权
             Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
             Uri uri = Uri.fromParts("package", getPackageName(), null);
@@ -138,18 +159,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             // 不需要解释为何需要该权限，直接请求授权
-            ActivityCompat.requestPermissions(MainActivity.this,
+            ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.CALL_PHONE},
                     1);
         }
     }
 
     public void onInternalButtonClick(View view) {
-
+        savePeriod();
     }
-
 
     public void onCancelCallButtonClicked(View view) {
         mMakePhoneCallThreadPool.shutdown();
+        AppSystem.out.printt(this, "暂停拨号成功");
+    }
+
+    private void savePhoneNum() {
+        mPreferences.edit()
+                .putString("phoneNum", String.valueOf(mBinding.telphoneNumEditText.getText()).trim())
+                .apply();
+        AppSystem.out.printt(this, "电话号码保存成功");
+    }
+
+    private void savePeriod() {
+        mPreferences.edit()
+                .putLong("period", Long.parseLong(String.valueOf(mBinding.periodEditText.getText()).trim()))
+                .apply();
+        AppSystem.out.printt(this, "间隔时间保存成功");
     }
 }
